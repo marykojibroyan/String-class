41d4ba6f1af8d7089ee8a58b8b39f059826fec6d
marykojibroyan <133524614+marykojibroyan@users.noreply.github.com>
2023-12-11 20:44:54 +0400

    Update String.main.cpp

--- String.main.cpp
+++ String.main.cpp
@@ -2,37 +2,21 @@
 #include <iostream>
 
 int main() {
-    // Example usage of the String class
+    // Create strings
     String str1("Hello");
-    String str2("World");
+    String str2(" World");
 
-    String str3 = str1; // Copy constructor
-    String str4;
+    // Concatenate strings using operator+
+    String result = str1 + str2;
 
-    str4 = str2; // Copy assignment operator
+    // Display result
+    std::cout << "Result: " << result << " (Size: " << result.Size() << ")\n";
 
-    std::cout << "str1: ";
-    str1.display();
-    std::cout << ", Size: " << str1.getSize() << std::endl;
+    // Concatenate with a C-style string
+    result += "!!!";
 
-    std::cout << "str2: ";
-    str2.display();
-    std::cout << ", Size: " << str2.getSize() << std::endl;
-
-    std::cout << "str3: ";
-    str3.display();
-    std::cout << ", Size: " << str3.getSize() << std::endl;
-
-    std::cout << "str4: ";
-    str4.display();
-    std::cout << ", Size: " << str4.getSize() << std::endl;
-
-    // Append and display
-    str1.append(" ");
-    str1.append("World");
-    std::cout << "Appended str1: ";
-    str1.display();
-    std::cout << ", Size: " << str1.getSize() << std::endl;
+    // Display modified result
+    std::cout << "Modified Result: " << result << " (Size: " << result.Size() << ")\n";
 
     return 0;
 }
